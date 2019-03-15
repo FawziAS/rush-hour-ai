67815320ff6e3613cbd73a92954cf6fb17060197
Hanan Khoury <khouryhanan97@gmail.com>
2019-03-15 11:48:40 +0200

    added time limit to IDS algorithm

--- ai/algorithms/IDS.py
+++ ai/algorithms/IDS.py
@@ -1,17 +1,26 @@
+import time
+
+import timestamp as timestamp
+
 from ai.algorithms.DLS import DLS
 
 
 class IDS:
+    start_time = 0
 
     @staticmethod
-    def start_ids(initial_state):
+    def start_ids(initial_state, time_limit):
+        IDS.start_time = time.time()
         i = 1
-        while i<10000:
+        while time.time() - IDS.start_time < time_limit:
             solution_state = DLS.start_dls(initial_state, i)
             if solution_state is None:
                 i += 1
+                continue
             else:
                 print("Solution: " + solution_state.get_solution_path() + solution_state.get_last_move())
                 break
-        print("IDS Failed to solve")
+
+        print("IDS Failed to solve problem")
         DLS.reset_stack()
+        IDS.start_time = 0
